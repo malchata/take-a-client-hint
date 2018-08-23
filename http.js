e6fa306354c0f43e0753f7c51425afe0821df1ca
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2018-08-23 00:08:20 -0500

    More slides.

--- http.js
+++ http.js
@@ -5,4 +5,4 @@ const htdocs = path.join(__dirname, "htdocs");
 
 // Run static server
 app.use(express.static(htdocs));
-app.listen(8080);
+app.listen(8080, () => console.log("Presentation server running at http://localhost:8080"));
